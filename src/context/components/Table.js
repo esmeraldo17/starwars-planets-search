25c74635cc9f9950d91e2df2db8b32b8abaf9b81
esmeraldo17 <esmeraldoalberto55@gmail.com>
2022-10-20 21:52:55 +0100

    requisitos 3 e 4

--- src/context/components/Table.js
+++ src/context/components/Table.js
@@ -5,14 +5,46 @@ function Table() {
   const { apiList } = useContext(PlanetContext);
   const [list, setList] = useState(apiList);
   const [inputValue, setInputValue] = useState('');
+  const [inputColumn, setInputColumn] = useState('population');
+  const [inputOperator, setInputOperator] = useState('maior que');
+  const [inputNumber, setInputNumber] = useState('0');
   useEffect(() => {
     const arrfilter = apiList.filter((e) => e.name.includes(inputValue));
     setList(arrfilter);
   }, [apiList, inputValue]);
 
-  const handleChange = ({ target }) => {
+  const onSearchInputChange = ({ target }) => {
     const { value } = target;
     setInputValue(value);
+  };
+
+  const onInputColumnChange = ({ target }) => {
+    const { value } = target;
+    setInputColumn(value);
+  };
+  const onInputOperatorChange = ({ target }) => {
+    const { value } = target;
+    setInputOperator(value);
+  };
+
+  const onInputNumberChange = ({ target }) => {
+    const { value } = target;
+    setInputNumber(value);
+  };
+
+  const onFilterBtnClick = () => {
+    if (inputOperator === 'maior que') {
+      const arfilter = list.filter((e) => +e[inputColumn] > +inputNumber);
+      setList(arfilter);
+    }
+    if (inputOperator === 'menor que') {
+      const arfilter = list.filter((e) => +e[inputColumn] < +inputNumber);
+      setList(arfilter);
+    }
+    if (inputOperator === 'igual a') {
+      const arfilter = list.filter((e) => +e[inputColumn] === +inputNumber);
+      setList(arfilter);
+    }
   };
   return (
     <div>
@@ -20,8 +52,48 @@ function Table() {
         <input
           type="text"
           data-testid="name-filter"
-          onChange={ handleChange }
+          onChange={ onSearchInputChange }
         />
+        <label htmlFor="clFilter">
+          Coluna:
+          <select
+            id="clFilter"
+            data-testid="column-filter"
+            onChange={ onInputColumnChange }
+          >
+            <option value="population">population</option>
+            <option value="orbital_period">orbital_period</option>
+            <option value="diameter">diameter</option>
+            <option value="rotation_period">rotation_period</option>
+            <option value="surface_water">surface_water</option>
+          </select>
+        </label>
+        <label htmlFor="cpFilter">
+          Operador:
+          <select
+            id="cpFilter"
+            data-testid="comparison-filter"
+            onChange={ onInputOperatorChange }
+          >
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+        </label>
+        <input
+          type="number"
+          data-testid="value-filter"
+          value={ inputNumber }
+          onChange={ onInputNumberChange }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ onFilterBtnClick }
+
+        >
+          Filtrar
+        </button>
       </form>
       <table>
         <thead>
